4204e8882ac840464cd4359ad8b68770dfd16e13
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-09 12:14:09 +0000

    Images play their respective sounds on click

--- drumkitAssignment/main.js
+++ drumkitAssignment/main.js
@@ -1,24 +1,42 @@
 let soundNames = ["boom","clap","hihat","kick","openhat","ride","snare","tink","tom"];
 let soundFiles = [];
 let imageFiles = [];
-let imageNames = ["bigdrum", "cymbal", "randrum", "smalldrum", "mediumdrum", "othercymbal"];
 
-for (sound of soundNames) {
-    soundFiles.push(new Audio(`./sounds/${sound}.wav`));
+let container = document.getElementById("content");
+
+let instruments = {
+    cymbal: ["openhat", "hihat"],
+    smalldrum: ["snare"],
+    bigdrum: ["boom", "kick"],
+    mediumdrum: ["tom"],
+    othercymbal: ["ride"],
+    randrum: soundNames,
 }
 
 const chooseRandom = (list) => {
     return list[ Math.floor(Math.random()*list.length) ]
 }
 
-document.addEventListener('click', (e) => {
-    chooseRandom(soundFiles).play();
-});
-
-imageString = ""
-
-for (image of imageNames) {
-    imageString += `<img src="./images/${image}.png">`
+// Go through instruments and replace filenames with the audio objects
+// and add the respective image files.
+for (let i in instruments) {
+    let instrument = instruments[i];
+    for (let s in instrument) {
+        instrument[s] = new Audio(`./sounds/${instrument[s]}.wav`);
+    }
+    
+    imageHolder = document.createElement("div");
+    imageHolder.classList.add("imageHolder");
+    image = document.createElement("img");
+    image.src = `./images/${i}.png`
+    image.classList.add(i, "instrument");
+    image.addEventListener("click", (e) => {
+        let sound = chooseRandom(instruments[e.target.classList[0]])
+        // Without this, the sound has to completely finish before it can be played again
+        sound.currentTime = 0; 
+        sound.play();
+        e.target.style.scale = 0.8;
+    });
+    imageHolder.appendChild(image);
+    container.appendChild(imageHolder);
 }
-
-document.getElementsByTagName("main")[0].innerHTML = imageString;
