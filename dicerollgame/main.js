00cce042bf50b00a98bea46ed6d63466b77fe34f
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-10 16:59:57 +0000

    Replay function added

--- dicerollgame/main.js
+++ dicerollgame/main.js
@@ -1,12 +1,3 @@
-/* Requirements
-
-Dice button
-Hold button
-Score display
-"Playmat"
-
-*/
-
 const pipStrings = [
     "    x    ",
     "x       x",
@@ -16,13 +7,17 @@ const pipStrings = [
     "x xx xx x",
 ]
 
-let playerScore = null;
 let playmat = null;
+let replayBtn = document.getElementById("replayBtn");
 
 let gameEnded = null;
 
 const initialise = () => {
+    if (playmat != null) playmat.object.remove();
+
     gameEnded = false;
+
+    replayBtn.classList.add("hidden");
 
     playmat = {    
         object: document.getElementsByClassName("playmat")[0].cloneNode(true),
@@ -38,39 +33,41 @@ const initialise = () => {
 
     playmat.dice.pips = playmat.dice.face.getElementsByClassName("pip");
 
-    for (p of playmat.dice.pips) console.log(p);
-
     playmat.dice.object.classList.remove("lost");
     playmat.scoreDisplay.textContent = playmat.score;
+
+    playmat.dice.object.addEventListener("click", diceRoll);
+    document.getElementById("container").appendChild(playmat.object);
 }
 
 const diceRoll = () => {
-    
-    playmat.dice.object.style.position = "2px";
-
     if (gameEnded) return;
     let rollScore = Math.ceil( Math.random() * 6 );
-
-    console.log(rollScore);
 
     setPips(playmat.dice.pips, rollScore);
     playmat.score += rollScore;    
 
     if (rollScore == 1) {
-        gameEnded = true;
+        endGame(false)
         playmat.dice.object.classList.add("lost");
         return;
     }
 
     playmat.scoreDisplay.textContent = playmat.score;
-    if (playmat.score >= 20) gameEnded = true;
+    if (playmat.score >= 20) endGame(true);
+}
+
+const endGame = (isWin) => {
+    playmat.object.getElementsByClassName("scoreLabel")[0].textContent = "Final Score";
+    gameEnded = true;
+
+    replayBtn.classList.remove("hidden");
 }
 
 const setPips = (pipList, score) => {
     // Work out which pips need to be visible on the dice.
     // Compares against a list of strings at the top of the script.
     for (let i = 0; i < pipList.length; i++) {
-        console.log(pipList[i]);
         if (pipStrings[score-1][i] == "x") pipList[i].classList.remove("empty");
         else pipList[i].classList.add("empty");
     }
@@ -78,6 +75,4 @@ const setPips = (pipList, score) => {
 
 initialise();
 
-document.getElementById("container").appendChild(playmat.object);
-
-playmat.dice.object.addEventListener("click", diceRoll);
+replayBtn.addEventListener("click", initialise);
